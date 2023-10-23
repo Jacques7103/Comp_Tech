4bedd49eceacfef8d487d71571f649374f059fe3
Jacques7103 <ferdinand.jacques@binus.ac.id>
2023-10-23 22:50:06 +0700

    Added quiz num 4

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
-def keyword_count(file):
-    keywords = ["int", "float", "while", "main", "const"]
-    key_count = {key: 0 for key in keywords}
-    
-    keyword_buffer = ""
-    
-    for char in file:
-        if char.isalpha():
-            keyword_buffer += char
-        else:
-            if keyword_buffer in keywords:
-                key_count[keyword_buffer] += 1
-            keyword_buffer = ""
-    
-    return key_count
-
 file = """int main(){
             const float payment = 384.00
-            while (true) {
-                int x = 10;
-            }
         }"""
 
-keyword_counts = keyword_count(file)
+def keyword(file):
+    key_list = []
+    file = file.split()
+    for word in file:
+        words = ''.join(filter(str.isalpha, word))
+        if word[0] == "i":
+            if word[1] == "n":
+                if word[2] == "t" and words[-1] == "t":
+                    key_list.append("int")
+        elif word[0] == "f":
+            if word[1] == "l":
+                if word[2] == "o":
+                    if word[3] == "a":
+                        if word[4] == "t" and words[-1] == "t":
+                            key_list.append("float")
+        elif word[0] == "w":
+            if word[1] == "h":
+                if word[2] == "i":
+                    if word[3] == "l":
+                        if word[4] == "e" and words[-1] == "e":
+                            key_list.append("while")
+        elif word[0] == "m":
+            if word[1] == "a":
+                if word[2] == "i":
+                    if word[3] == "n" and words[-1] == "n":
+                        key_list.append("main")
+        elif word[0] == "c":
+            if word[1] == "o":
+                if word[2] == "n":
+                    if word[3] == "s":
+                        if word[4] == "t" and words[-1] == "t":
+                            key_list.append("const")
+    return key_list
 
-for key, count in keyword_counts.items():
-    print(f"'{key}' appears {count} times.")
+print(keyword(file))
